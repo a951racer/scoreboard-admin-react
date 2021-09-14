e3a2bf03e1bfdab8792e2220e1157f25a6f2605a
Stone Hobbs <stone@minnykid.com>
2021-09-13 20:06:54 -0500

    built login feature and did general cleanup

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,25 +1,53 @@
+import user from '../../API/user.js';
 import '../../css/bootstrap-4.4.1.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-function Auth() {
+async function loginUser(email, password) {
+    let userData;
+    try {
+        userData = await user.login(email, password);
+        return userData;
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            //incorrect username/password
+        }
+        return null;
+    }
+}
+
+function Auth({ setUserData }) {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const loginData = await loginUser(email, password);
+        if (loginData) {
+            setUserData(loginData);
+        }
+    }
+
     return (
-        <body style={{ paddingBottom: 70 }}>
-            <div class="jumbotron">
-                <h1 class="display-4 text-center">Game Night Scoreboard</h1>
-                <p class="lead text-center">Family game night just got high-tech!</p>
+        <div style={{ paddingBottom: 70 }}>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center">Game Night Scoreboard</h1>
+                <p className="lead text-center">Family game night just got high-tech!</p>
             </div>
-            <div class="container">
-                <form method="post" name="form1" class="text-center" id="login">
+            <div className="container">
+                <form method="post" name="form1" className="text-center" id="login" onSubmit={handleSubmit}>
                     <div>
-                        <label>Username: </label><input type="email" />
+                        <label>Username: </label><input type="email" onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div>
-                        <label>Password: </label><input type="password" />
+                        <label>Password: </label><input type="password" onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    <button type="button" class="btn btn-primary">Login</button>
+                    <button type="submit" className="btn btn-primary">Login</button>
                 </form>
             </div>
-            <nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light align-content-center">
-                <div class="container">
+            <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-light align-content-center">
+                <div className="container">
                     A HobbSquad Joint - Copyright ® 2021
                 </div>
             </nav>
@@ -28,8 +56,12 @@ function Auth() {
 
             <script src="js/popper.min.js"></script>
             <script src="js/bootstrap-4.4.1.js"></script>
-        </body>
+        </div>
     );
 }
+
+Auth.propTypes = {
+    setUserData: PropTypes.func.isRequired
+};
 
 export default Auth;
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -2,20 +2,20 @@ import '../../css/bootstrap-4.4.1.css';
 
 function Main() {
   return (
-    <body style={{ paddingBottom: 70 }}>
-      <div class="jumbotron">
-        <h1 class="display-4 text-center">Game Night Scoreboard</h1>
-        <p class="lead text-center">Family game night just got high-tech!</p>
+    <div style={{ paddingBottom: 70 }}>
+      <div className="jumbotron">
+        <h1 className="display-4 text-center">Game Night Scoreboard</h1>
+        <p className="lead text-center">Family game night just got high-tech!</p>
       </div>
 
 
-      <div class="container">
+      <div className="container">
 
-        <div class="container" style={{ marginBottom: 35 }}>
-          <button type="button" class="btn btn-primary">New Game</button>
+        <div className="container" style={{ marginBottom: 35 }}>
+          <button type="button" className="btn btn-primary">New Game</button>
         </div>
 
-        <div class="container">
+        <div className="container">
           <h3>Active Games</h3>
           <table width="400" border="1">
             <tbody>
@@ -25,12 +25,12 @@ function Main() {
                 <th scope="col">Players</th>
               </tr>
               <tr>
-                <td><a href="#">8/1/2021 1:30pm</a></td>
+                <td><a href="/">8/1/2021 1:30pm</a></td>
                 <td>Oh Hell</td>
                 <td>6</td>
               </tr>
               <tr>
-                <td><a href="#">8/7/2021 2:10pm</a></td>
+                <td><a href="/">8/7/2021 2:10pm</a></td>
                 <td>Hearts</td>
                 <td>5</td>
               </tr>
@@ -38,7 +38,7 @@ function Main() {
           </table>
         </div>
         <br />
-        <div class="container">
+        <div className="container">
           <h3>Recent Games</h3>
           <table width="400" border="1">
             <tbody>
@@ -48,22 +48,22 @@ function Main() {
                 <th scope="col">Players</th>
               </tr>
               <tr>
-                <td><a href="#">7/27/2021 12: 30pm</a></td>
+                <td><a href="/">7/27/2021 12: 30pm</a></td>
                 <td>5 Crowns</td>
                 <td>5</td>
               </tr>
               <tr>
-                <td><a href="#">7/21/2021 3: 15m</a></td>
+                <td><a href="/">7/21/2021 3: 15m</a></td>
                 <td>Hearts</td>
                 <td>6</td>
               </tr>
               <tr>
-                <td><a href="#">7/10/2021 1: 25pm</a></td>
+                <td><a href="/">7/10/2021 1: 25pm</a></td>
                 <td>Phase 10</td>
                 <td>4</td>
               </tr>
               <tr>
-                <td><a href="#">7/4/2021 2: 30pm</a></td>
+                <td><a href="/">7/4/2021 2: 30pm</a></td>
                 <td>5 Crowns</td>
                 <td>7</td>
               </tr>
@@ -72,8 +72,8 @@ function Main() {
         </div>
 
 
-        <nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light align-content-center">
-          <div class="container">
+        <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-light align-content-center">
+          <div className="container">
             A HobbSquad Joint - Copyright ® 2021
           </div>
         </nav>
@@ -83,7 +83,7 @@ function Main() {
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap-4.4.1.js"></script>
       </div>
-    </body>
+    </div>
   );
 }
 
